4792836d3df0106bdc499ad235ab71336a09c144
DESKTOP-0OO649U\Admin <thandarayetopcom@gmail.com>
2024-01-13 12:06:24 +0630

    edit front

--- resources/views/articles/add.blade.php
+++ resources/views/articles/add.blade.php
@@ -1,43 +1,42 @@
-@extends("layouts.app")
+@extends('layouts.app')
 
-@section("content")
+@section('content')
+    <div class="container" style="max-width: 800px">
 
-<div class="container" style="max-width: 800px">
-
-    @if($errors->any())
-        <div class="alert alert-warning">
-            @foreach ($errors->all() as $error)
-                {{$error}}
-            @endforeach
-        </div>
-    @endif
-    
-    <form action="" method="post">
-        @csrf
-
-        <div class="mb-2">
-            <label for="">Title</label>
-            <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" value="{{old('title')}}">
-            @error('title')
-             <div class="invalid-feedback">
-                {{$message}}
-             </div>
-                
-            @enderror
-        </div>
-        <div class="mb-2">
-            <label for="">Body</label>
-            <textarea name="body" class="form-control"></textarea>
-        </div>
-        <div class="mb-2">
-            <label for="">Category</label>
-            <select type="text" name="category_id" class="form-select">
-                @foreach ($categories as $category)
-                    <option value="{{$category->id}}">{{$category->name}}</option>
+        @if ($errors->any())
+            <div class="alert alert-warning">
+                @foreach ($errors->all() as $error)
+                    {{ $error }}
                 @endforeach
-            </select>
-        </div>
-        <button class="btn btn-primary">Add Article</button>
-    </form>
-</div>
+            </div>
+        @endif
+
+        <form action="" method="post">
+            @csrf
+
+            <div class="mb-2">
+                <label for="">Title</label>
+                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror"
+                    value="{{ old('title') }}">
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-2">
+                <label for="">Body</label>
+                <textarea name="body" class="form-control"></textarea>
+            </div>
+            <div class="mb-2">
+                <label for="">Category</label>
+                <select type="text" name="category_id" class="form-select">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <button class="btn btn-primary">Add Article</button>
+        </form>
+    </div>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,30 @@
 <?php
 
+use App\Http\Controllers\CategoryApiController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::apiResource('/categories',CategoryApiController::class);
+
+Route::post('/login',function(){
+    $email = request()->email;
+    $password = request()->password;
+
+    if(!$email or !$password){
+        return response(['msg'=>'email and password required'],403);
+    }
+
+    $user = User::where('email',$email)->first();
+    // return $user;
+    if($user){
+        if(password_verify($password,$user->password)){
+            return $user->createToken('api')->plainTextToken;
+        }
+    }
+    return response(['msg'=> 'incorrect email and password'], 403);
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
